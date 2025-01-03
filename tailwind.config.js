51145f5c0af22a1b06afc6cca26023c64a993150
NuriddinJamshed <j.nuriddin2021@gmail.com>
2025-01-03 22:01:21 +0500

    Header ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '320px',
+        'md': '575px',
+        'lg' : '767px',
+        'xl' : '992px',
+        '2xl' : '1500px',
+      }
+    },
   },
   plugins: [],
 }
